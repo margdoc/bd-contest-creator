4fa1ef89cc5cfe9567fb68f3311909fcec37b952
margdoc <mikgrzebieluch@gmail.com>
2021-01-19 21:36:42 +0100

    Fixing

--- src/action.tsx
+++
@@ -1,9 +0,0 @@
-interface LoginAction {
-    type: "LoginAction";
-    email: string;
-    password: string;
-}
-
-export type Action = LoginAction;
-
-export type Dispatcher<A extends Action = Action> = (action: A) => void;
